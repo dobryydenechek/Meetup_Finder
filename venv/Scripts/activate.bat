2912ace773ebae61651307ffec8db536c328de54
dobryydenechek <kitte246@gmail.com>
2020-03-23 22:13:49 +0300

    ..

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Катя\Desktop\MeetupFinder\venv
+set VIRTUAL_ENV=C:\Users\Катя\Desktop\MeetupFinder\Meetup_Finder\venv
 
 if not defined PROMPT set PROMPT=$P$G
 

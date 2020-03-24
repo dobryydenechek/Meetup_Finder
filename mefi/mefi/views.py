4fbe0e3b2caafba585e7bcb0baf3478d78f82ff0
dobryydenechek <kitte246@gmail.com>
2020-03-24 10:35:53 +0300

    вход на сайт и  регистрация
    при входе на сайт пока что нужно указывать ФИО вместо почты

--- mefi/mefi/views.py
+++ mefi/mefi/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 class HomeView(TemplateView):
     template_name = "home.html"
@@ -16,7 +17,30 @@ class LoginView(TemplateView):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("/")
+                return redirect("/accounts/profile")
             else:
                 context['error'] = "Логин или пароль неправильные"
         return render(request, self.template_name, context)
+
+class ProfilePage(TemplateView):
+    template_name = "registration/profile.html"
+
+class RegisterView(TemplateView):
+    template_name = "registration/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            usersn = request.POST.get('usersn')
+            username = request.POST.get('username')
+            userfn = request.POST.get('userfn')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            password2 = request.POST.get('password2')
+
+            if password == password2:
+                full_name_user = usersn + ' ' + username + ' ' + userfn
+                User = get_user_model()
+                User.objects.create_user(full_name_user, email, password)
+                return redirect(reverse("login"))
+
+        return render(request, self.template_name)

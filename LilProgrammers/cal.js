aab55fc54715803f08e545dc3dcea0a30ea7aaac
deadlive <legendary2002k@gmail.com>
2020-04-09 10:29:01 +0300

    fix css

--- LilProgrammers/cal.js
+++ LilProgrammers/cal.js
@@ -1,4 +1,3 @@
-
 var tags = document.getElementById("tags");
 var buttonsave = document.getElementById("buttonsave");
 var buttonedit = document.getElementById("buttonedit");
@@ -6,39 +5,34 @@ var buttonnext = document.getElementById("buttonnext");
 var buttonprev = document.getElementById("buttonprev");
 var month = document.getElementById("month");
 var year = document.getElementById("year");
-var months = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-var cells =document.getElementById("cal").getElementsByTagName("td"); 
-var events ={};
-
+var months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+var cells = document.getElementById("cal").getElementsByTagName("td");
+var events = {};
 var cm = new Date().getMonth();
-var cy = new Date().getFullYear()
-
-
-
-
-function getday(y,m){
-    var day = new Date(y,m,1).getDay()
+var cy = new Date().getFullYear();
+var tagsdata ={};
+var tagschek =[];
+function getday(y, m) {
+    var day = new Date(y, m, 1).getDay()
     day--
-    if(day<0){
-        day=6
+    if (day < 0) {
+        day = 6
     }
     return day
 }
 
-function getmonthdays (y,m){
-    return 32 - new Date(y,m, 32).getDate();
-    }
+function getmonthdays(y, m) {
+    return 32 - new Date(y, m, 32).getDate();
+}
 
-
-
-function update(){
+function update() {
     let ida = "data-ida"
-    month.innerText=months[cm];
-    year.innerText=cy;
+    month.innerText = months[cm];
+    year.innerText = cy;
     for (let index = 0; index < cells.length; index++) {
         const cell = cells[index];
         cell.removeAttribute(ida);
-        cell.innerText="";
+        cell.innerText = "";
         cell.classList.remove("today");
         let ovs = cell.getElementsByClassName("event");
         for (let index = 0; index < ovs.length; index++) {
@@ -46,79 +40,101 @@ function update(){
             cell.removeChild(ov);
         }
     }
-
-    let start =getday(cy,cm)
-    for (let index = start; index < getmonthdays(cy,cm)+start; index++) {
+    
+    let start = getday(cy, cm);
+    for (let index = start; index < getmonthdays(cy, cm) + start; index++) {
         const cell = cells[index];
-        cell.innerText=index-start+1;
-        let idday =cy+"-"+(cm+1)+"-"+(index-start+1);
-        cell.setAttribute(ida,idday);
-        let ov = events[idday];
-        if(ov){
-            for (let index = 0; index <ov.length; index++) {
-                const element = ov[index];
-                var ne =document.createElement("span");
-                ne.innerText=element.name;
-                ne.classList.add("event");
-                ne.setAttribute(ida,idday);
-                ne.setAttribute("data-ei",index)
-                cell.appendChild(ne);
-            }
-            uptippy()
+        cell.innerText = index - start + 1;
+        let idday = cy + "-" + (cm + 1) + "-" + (index - start + 1);
+        cell.setAttribute(ida, idday);
+    }
+    if (cy == new Date().getFullYear() && cm == new Date().getMonth()) {
+        cells[new Date().getDate() + start - 1].classList.add("today")
+    }
+    let promis = axios.get(`events/${cy}-${cm+1}.json`);
+    promis.then(function(respons){
+        for (let index = 0; index < respons.data.length; index++) {
+            const event = respons.data[index];
+            events[event.id]=event;
+            let ne = document.createElement("span");
+            ne.innerText = event.title;
+            ne.classList.add("event");
+            ne.setAttribute(ida, event.date);
+            ne.setAttribute("data-ei",event.id)
+            cells[new Date(event.date).getDate() + start - 1].appendChild(ne);
         }
-    }
-    if(cy==new Date().getFullYear() && cm==new Date().getMonth()){
-        cells[ new Date().getDate()+start-1].classList.add("today")
-    }
+        uptippy()
+        filter()
+    })
 }
 
-function upteg(){
+function upteg() {
     var promis = axios.get("tegs.json");
-        promis.then(function(response){
-        console.log(response.data);
+    promis.then(function (response) {
         var tags = document.getElementById("tags");
-        tags.innerHTML="";
+        tags.innerHTML = "";
         for (let index = 0; index < response.data.length; index++) {
             const teg = response.data[index];
-            var li =document.createElement("li");
+            var li = document.createElement("li");
             li.classList.add("tags");
-            li.innerHTML='<label class="container font"><span class="svap">     </span><input type="checkbox"><span class="checkmark"></span></label>';
-            li.getElementsByClassName("svap")[0].innerText=teg.tl_title;
-            tags.appendChild(li)
-            console.log(teg.tl_title)
-
+            li.setAttribute("data-id",teg.id);
+            li.innerHTML = '<label class="container font"><span class="svap">     </span><input type="checkbox"><span class="checkmark"></span></label>';
+            li.getElementsByClassName("svap")[0].innerText = teg.tl_title;
+            tags.appendChild(li);
+            tagsdata[teg.id]=teg;
         }
-  })
+    })
 }
 
-
-
-
-
-
-
-
-function nextmonth(){
+function nextmonth() {
     cm++;
-    if(cm>11){
-        cm=0;
+    if (cm > 11) {
+        cm = 0;
         cy++
     }
     update();
 }
-function prevmonth(){
+
+function prevmonth() {
     cm--;
-    if(cm<0){
-        cm=11;
+    if (cm < 0) {
+        cm = 11;
         cy--
     }
     update();
 }
+function filter(){
+    
+    let calevents=document.getElementsByClassName("event")
+    
+    for (let index = 0; index < calevents.length; index++) {
+        const element = calevents[index];
+        element.classList.remove("hidden")
+        if(!tagschek.length){
+            continue
+        }
+        let eventdata = events[element.getAttribute("data-ei")];
+        let flag =false;
+        
+        for (let index = 0; index < eventdata.tags.length; index++) {
+            const id = eventdata.tags[index];
 
+            for (let index = 0; index < tagschek.length; index++) {
+                const chek = tagschek[index];
+                if(chek==id){
+                    flag=true;
+                    break
+                }
+            }
+            if (flag){break}
+        }
+        if(!flag){
+            element.classList.add("hidden")
+        }
+    }
+}
 
 function save() {
-
-
     var chek = 0;
     for (let index = 0; index < tags.children.length; index++) {
         const li = tags.children[index];
@@ -131,18 +147,20 @@ function save() {
         alert("Иди нахуй с такими тегами");
         return
     }
+    tagschek=[];
     for (let index = 0; index < tags.children.length; index++) {
         const li = tags.children[index];
         const chekbox = li.getElementsByTagName("input")[0];
         if (chekbox.checked) {
             chekbox.disabled = true;
+            tagschek.push(li.getAttribute("data-id"))
         } else {
             li.hidden = true
-
         }
     }
     buttonsave.hidden = true;
     buttonedit.hidden = false;
+    filter()
 }
 
 function edit() {
@@ -155,38 +173,54 @@ function edit() {
     buttonsave.hidden = false;
     buttonedit.hidden = true;
 }
-//
 
-
-
-function uptippy(){
-  tippy('#cal td .event', {
-    content:`
-    
+function uptippy() {
+    tippy('#cal td .event', {
+        content:
+     `
     <div class="detail" > 
-      <h4 class="dataevent"></h4><button onclick="window.location='#mailing'">Я пойду</button>
+      <h4 class="dataevent"></h4>
+      <p class="time"></p>
+      <p class="description"></p>
+      <span class="city"></span>,
+      <span class="streetname"></span>,
+      <span class="houseNumber"></span>,
+      <span class="letter"></span>
+      <p class="placeName"></p>
+      <div class="datatags"> </div>
+      <button onclick="window.location='#mailing'">Я пойду</button>
     </div>
-    
     `,
-    allowHTML: true,
-    arrow:true,
-    trigger: 'click',
-    interactive: true,
-    theme:"detail",
-    onShow(instance){
-        let ida =instance.reference.getAttribute("data-ida");
-        let ei =instance.reference.getAttribute("data-ei");
-        let event = events[ida][ei];
-        instance.popper.getElementsByClassName("dataevent")[0].innerText=event.name;
-       
-    }
+        allowHTML: true,
+        arrow: true,
+        trigger: 'click',
+        interactive: true,
+        theme: "detail",
+        onShow(instance) {
+        
+            
+            let ei = instance.reference.getAttribute("data-ei");
+            let event = events[ei];
+            instance.popper.getElementsByClassName("dataevent")[0].innerText = event.title;
+            instance.popper.getElementsByClassName("description")[0].innerText = event.description;
+            instance.popper.getElementsByClassName("time")[0].innerText = event.time;
+            instance.popper.getElementsByClassName("city")[0].innerText = event.city;
+            instance.popper.getElementsByClassName("streetname")[0].innerText = event.streetname;
+            instance.popper.getElementsByClassName("houseNumber")[0].innerText = event.houseNumber;
+            instance.popper.getElementsByClassName("letter")[0].innerText = event.letter;
+            instance.popper.getElementsByClassName("placeName")[0].innerText = event.placeName;
+            instance.popper.getElementsByClassName("datatags")[0].innerHTML="";
+            for (let index = 0; index < event.tags.length; index++) {
+                
+                const tag = event.tags[index];
+                var span = document.createElement("span");
+                span.innerText = tagsdata[tag].tl_title;
+                instance.popper.getElementsByClassName("datatags")[0].appendChild(span);
+            }
+            
+        }
+    });
+}
 
-  });}
-
-
-
-
-events["2020-3-17"]=[{name:"НАзвание мероприятия"},
-{name:"Php Meetup"}];
 upteg()
 update()

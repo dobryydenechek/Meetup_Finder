34b339dd9871484b3b4597daacf19ecc4f08d642
Mark <44618739+L1gHtFox@users.noreply.github.com>
2020-10-11 00:27:06 +0300

    Ок, возвращаем

--- mefi/settings.py
+++ mefi/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'MeFi',
         'USER': 'postgres',
+        'PASSWORD': '';
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
